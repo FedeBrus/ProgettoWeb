24d9854da13fa8e8411ba1e07881a633c98793f4
Nikicoraz <nicolacorato05@gmail.com>
2026-05-07 11:33:15 +0200

    init intellij

---
+++ src/test/java/com/palestra/palestra/PalestraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palestra.palestra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalestraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
