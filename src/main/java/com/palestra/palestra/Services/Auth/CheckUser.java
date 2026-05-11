ac1977a2993b498b4fd521f38fb06ce6c4e048a3
Nikicoraz <nicolacorato05@gmail.com>
2026-05-11 15:18:11 +0200

    Modulo check user e aggiunta di un utente al DB

---
+++ src/main/java/com/palestra/palestra/Services/Auth/CheckUser.java
@@ -0,0 +1,25 @@
+package com.palestra.palestra.Services.Auth;
+
+import com.palestra.palestra.Repositories.UserRepository;
+import com.palestra.palestra.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckUser {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CheckUser(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean checkAndInsert(User u) {
+        if (userRepository.userExists(u.getUsername())) {
+            return false;
+        } else {
+            userRepository.addUser(u);
+            return true;
+        }
+    }
+}
