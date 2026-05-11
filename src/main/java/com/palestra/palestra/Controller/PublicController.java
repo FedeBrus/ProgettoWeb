964e6944f50db20f6a66ce9c9cbb63fe768559b7
Nikicoraz <nicolacorato05@gmail.com>
2026-05-11 11:33:45 +0200

    Pagina di login

--- src/main/java/com/palestra/palestra/Controller/PublicController.java
+++ src/main/java/com/palestra/palestra/Controller/PublicController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PublicController {
-
     @GetMapping("/")
     public String homepage(Model page){
         return "public/index";
+    }
+
+    @GetMapping("/login")
+    public String login(Model page) {
+        return "public/auth/login";
     }
 }
